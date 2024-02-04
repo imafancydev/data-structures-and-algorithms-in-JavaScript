001fd58b07fceaee14958e96db8f37546d015a2e
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-02-03 23:23:18 -0300

    feat: linked_list function

---
+++ linked_lists/linked_list.js
@@ -0,0 +1,20 @@
+function Node() {
+  return ({
+    element,
+    next: null
+  });
+}
+
+function LinkedList() {
+  return ({
+    insert,
+    find,
+    remove,
+    toString
+  });
+
+  function insert() { }
+  function find() { }
+  function remove() { }
+  function toString() { }
+}
