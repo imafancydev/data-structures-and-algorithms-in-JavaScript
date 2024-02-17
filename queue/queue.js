0356c6cba55783961ff57ec5db8f2ecce335d619
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-02-17 15:49:54 -0300

    feat: queue implemented

--- queue/queue.js
+++ queue/queue.js
@@ -1,1 +1,53 @@
+function Queue() {
+  let dataStore = [];
 
+  //Fifo first in first out
+
+  return ({
+    enqueue,
+    dequeue,
+    front,
+    end,
+    clear,
+    toString
+  });
+
+  function enqueue(value) {
+    return dataStore.unshift(value);
+  };
+
+  function dequeue() {
+    return dataStore.pop();
+  };
+
+  function front() {
+    return dataStore[0];
+  };
+
+  function end() {
+    return dataStore[dataStore.length - 1];
+  };
+
+  function clear() {
+    return dataStore = [];
+  };
+
+  function toString() {
+    console.log(dataStore);
+  }
+
+};
+
+let queue = Queue();
+
+queue.enqueue("Giovanna");
+queue.enqueue("Lucas");
+queue.enqueue("Tito");
+queue.enqueue("Titus");
+
+queue.dequeue();
+queue.dequeue();
+
+queue.toString();
+
+queue.clear();
